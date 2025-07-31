9a90a9b7196b9d21d31ceee3b784b72a770e0c52
Edwinnavia <edwinnavia24@gmail.com>
2025-07-31 19:14:18 -0400

    HU-17-TC120 test: Agregando validación de schema input y generación de datos aleatorios

--- tests/catalog/association_types/test_HU07_EN_POST_CreateProductAssociationTypes.py
+++ tests/catalog/association_types/test_HU07_EN_POST_CreateProductAssociationTypes.py
@@ -1,22 +1,77 @@
 import jsonschema
 import requests
 import pytest
+from faker import Faker
 from utils.config import BASE_URL
+
+faker = Faker()
 
 
 def test_TC120_Crear_tipo_de_asociacion_con_code_y_translations_en_US_name_validos_exitoso(auth_headers):
     url = f"{BASE_URL}/admin/product-association-types"
+    random_code = faker.uuid4()
+    random_name = faker.company()
+
     data = {
-        "code": "MtJHPlPVc8LsrLQdZSTD-3NrEBA_BWr_XYZ2",
+        "code": random_code,
         "translations": {
             "en_US": {
-                "name": "Colgate"
+                "name": random_name
             }
         }
     }
+
+    schema_input = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "code": {
+                "type": "string"
+            },
+            "translations": {
+                "type": "object",
+                "properties": {
+                    "en_US": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string"
+                            }
+                        },
+                        "required": ["name"]
+                    },
+                    "es_ES": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string"
+                            }
+                        }
+                    },
+                    "fr_FR": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                },
+                "required": ["en_US"],
+                "additionalProperties": False
+            }
+        },
+        "required": ["code", "translations"]
+    }
+
+    try:
+        jsonschema.validate(instance=data, schema=schema_input)
+    except jsonschema.ValidationError as err:
+        pytest.fail(f"Input data is not valid: {err}")
+
     response = requests.post(url, headers=auth_headers, json=data)
 
-    schema = {
+    schema_output = {
         "$schema": "http://json-schema.org/draft-04/schema#",
         "type": "object",
         "properties": {
@@ -59,6 +114,6 @@ def test_TC120_Crear_tipo_de_asociacion_con_code_y_translations_en_US_name_valid
 
     assert response.status_code == 201
     try:
-        jsonschema.validate(instance=response.json(), schema=schema)
+        jsonschema.validate(instance=response.json(), schema=schema_output)
     except jsonschema.ValidationError as err:
         pytest.fail(f"Response is not valid: {err}")
