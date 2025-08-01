36e9b23548a5750c259cba951ea36f998be03bde
Elv500 <elvis.alvarez.2001@gmail.com>
2025-08-01 16:46:26 -0400

    Agregando rutas para reutilizar los endpoints y refactorizar codigo

--- tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
+++ tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
@@ -2,13 +2,12 @@ import pytest
 
 from src.assertions.inventory_assertions import AssertionInventory
 from src.assertions.status_code_assertions import AssertionStatusCode
-from utils.config import BASE_URL
+from src.routes.endpoint_inventory import EndpointInventory
+from src.routes.request import SyliusRequest
 
 @pytest.mark.smoke
 @pytest.mark.regression
 def test_TC23_Obtener_lista_completa_de_fuentes_de_inventario(auth_headers):
-
-    url = f"{BASE_URL}/admin/inventory-sources"
-    response = requests.get(url, headers=auth_headers)
+    response = SyliusRequest.get(EndpointInventory.inventory(),auth_headers)
     AssertionStatusCode.assert_status_code_200(response)
     AssertionInventory.assert_inventory_list_schema(response.json())
