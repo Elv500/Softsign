ff82c9e9b61f1bbc18f6b2f24e5a67fdf59367c8
Elv500 <elvis.alvarez.2001@gmail.com>
2025-07-31 21:59:00 -0400

    Configurando src con Assert, Data y Resources: Inventory & Se agreego report-HTML

---
+++ src/resources/payloads/payload_inventory.py
@@ -0,0 +1,20 @@
+import json
+
+class PayloadInventory:
+
+    @staticmethod
+    def build_payload_add_inventory(data):
+        payload = {
+            "address": {
+                "countryCode": data.get("countryCode"),
+                "street": data.get("street"),
+                "city": data.get("city"),
+                "postcode": data.get("postcode")
+            },
+            "code": data.get("code"),
+            "name": data.get("name"),
+            "priority": data.get("priority", 0),
+            "channels": data.get("channels", [])
+        }
+
+        return json.dumps(payload, indent=4)
--- tests/catalog/inventory/test_HU05_EA_POST_Inventory.py
+++ tests/catalog/inventory/test_HU05_EA_POST_Inventory.py
@@ -1,147 +1,20 @@
 import pytest
 import requests
-import jsonschema
-
 from faker import Faker
 
 from utils.config import BASE_URL
+from src.assertions.inventory_assertions import AssertionInventory
+from src.assertions.status_code_assertions import AssertionStatusCode
+from src.data.inventory import generate_inventory_source_data
 
 faker = Faker()
 
 @pytest.mark.smoke
 def test_TC27_Crear_una_fuente_de_inventario_con_datos_validos(auth_headers):
-    """
-    Verificar que el sistema permite registrar correctamente una fuente nueva.
-    """
-
+    
     url = f"{BASE_URL}/admin/inventory-sources"
-
-    payload = {
-        "address": {
-            "countryCode": "US",
-            "street": "string",
-            "city": "string",
-            "postcode": "string"
-        },
-        "code": faker.slug(),
-        "name": "string",
-        "priority": 0,
-        "channels": [
-            "/api/v2/admin/channels/HOME_WEB"
-        ]
-    }
-
-    response = requests.post(url, headers=auth_headers, json=payload)
-
-    assert response.status_code == 201
-
-    #Validacion Squema Input
-    input_schema = {
-        "type": "object",
-        "required": [
-            "address",
-            "channels",
-            "code",
-            "name",
-            "priority"
-        ],
-        "properties": {
-            "address": {
-                "type": "object",
-                "required": [
-                    "city",
-                    "countryCode",
-                    "postcode",
-                    "street"
-                ],
-                "properties": {
-                    "countryCode": {
-                        "type": "string"
-                    },
-                    "street": {
-                        "type": "string"
-                    },
-                    "city": {
-                        "type": "string"
-                    },
-                    "postcode": {
-                        "type": "string"
-                    }
-                }
-            },
-            "code": {
-                "type": "string"
-            },
-            "name": {
-                "type": "string"
-            },
-            "priority": {
-                "type": "integer"
-            },
-            "channels": {
-                "type": "array",
-                "items": {
-                    "type": "string"
-                }
-            }
-        }
-    }
-
-    try:
-        jsonschema.validate(payload, input_schema)
-    except jsonschema.ValidationError as e:
-        pytest.fail(f"JSON schema input doesnt match {e}")
-
-
-    #Validacion Schema output
-    output_schema = {
-        "type": "object",
-        "required": [
-            "@context",
-            "@id",
-            "@type",
-            "address",
-            "channels",
-            "code",
-            "id",
-            "name",
-            "priority"
-        ],
-        "properties": {
-            "@context": {
-                "type": "string"
-            },
-            "@id": {
-                "type": "string"
-            },
-            "@type": {
-                "type": "string"
-            },
-            "id": {
-                "type": "integer"
-            },
-            "address": {
-                "type": "string"
-            },
-            "code": {
-                "type": "string"
-            },
-            "name": {
-                "type": "string"
-            },
-            "priority": {
-                "type": "integer"
-            },
-            "channels": {
-                "type": "array",
-                "items": {
-                    "type": "string"
-                }
-            }
-        }
-    }
-
-    try:
-        jsonschema.validate(response.json(), output_schema)
-    except jsonschema.ValidationError as e:
-        pytest.fail(f"JSON schema output doesnt match {e}")
+    data = generate_inventory_source_data()
+    response = requests.post(url, headers=auth_headers, json=data)
+    AssertionInventory.assert_inventory_add_input_schema(data)
+    AssertionStatusCode.assert_status_code_201(response)
+    AssertionInventory.assert_inventory_add_output_schema(response.json())
