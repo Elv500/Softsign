f4dcb411ec50dd7768f78b0ddeeec64aa2b51268
Elv500 <elvis.alvarez.2001@gmail.com>
2025-08-01 16:46:26 -0400

    Agregando rutas para reutilizar los endpoints y refactorizar codigo

---
+++ src/routes/request.py
@@ -0,0 +1,28 @@
+import requests
+
+class SyliusRequest:
+    @staticmethod
+    def get(url, headers):
+        response = requests.get(url, headers=headers)
+        return response
+
+    @staticmethod
+    def post(url, headers, payloads=None):
+        headers = headers.copy()
+        headers.update({'Content-Type': 'application/json'})
+        response = requests.post(url, headers=headers, json=payloads)
+        return response
+
+    @staticmethod
+    def put(url, headers, payloads=None):
+        headers = headers.copy()
+        headers.update({'Content-Type': 'application/json'})
+        response = requests.put(url, headers=headers, json=payloads)
+        return response
+
+    @staticmethod
+    def delete(url, headers, payloads=None):
+        headers = headers.copy()
+        headers.update({'Content-Type': 'application/json'})
+        response = requests.delete(url, headers=headers, json=payloads)
+        return response
