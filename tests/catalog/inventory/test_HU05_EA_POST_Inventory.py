07fd3b2093be9252d314ab61a5961c6fd4d6cb6d
Enrique Delgadillo <202000658@est.umss.edu>
2025-08-02 12:36:33 -0400

    HU02-TC114 test: Verificaion de codigo 201 de schema

--- tests/catalog/inventory/test_HU05_EA_POST_Inventory.py
+++ tests/catalog/inventory/test_HU05_EA_POST_Inventory.py
@@ -1,18 +1,20 @@
 import pytest
+import requests
+from faker import Faker
 
+from utils.config import BASE_URL
 from src.assertions.inventory_assertions import AssertionInventory
 from src.assertions.status_code_assertions import AssertionStatusCode
-from src.routes.endpoint_inventory import EndpointInventory
-from src.routes.request import SyliusRequest
 from src.data.inventory import generate_inventory_source_data
 
+faker = Faker()
+
 @pytest.mark.smoke
-def test_TC27_Crear_una_fuente_de_inventario_con_datos_validos(setup_add_inventory):
-    headers, created_inventories = setup_add_inventory
+def test_TC27_Crear_una_fuente_de_inventario_con_datos_validos(auth_headers):
+    
+    url = f"{BASE_URL}/admin/inventory-sources"
     data = generate_inventory_source_data()
-    response = SyliusRequest.post(EndpointInventory.inventory(), headers, data)
+    response = requests.post(url, headers=auth_headers, json=data)
     AssertionInventory.assert_inventory_add_input_schema(data)
     AssertionStatusCode.assert_status_code_201(response)
     AssertionInventory.assert_inventory_add_output_schema(response.json())
-    created_inventory = response.json()
-    created_inventories.append(created_inventory)
