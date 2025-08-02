3e3065cec7bf195f30b010f21ef9b9e0c0db180a
Elv500 <elvis.alvarez.2001@gmail.com>
2025-08-02 17:48:20 -0400

    Automatizando TC´s de login

--- tests/login/test_HU00_EA_POST_LoginAdmin.py
+++ tests/login/test_HU00_EA_POST_LoginAdmin.py
@@ -1,25 +1,49 @@
 import requests
 import pytest
 
-import jsonschema
-from jsonschema.exceptions import ValidationError
-
 from src.routes.endpoint import Endpoint
 from src.routes.request import SyliusRequest
 from src.assertions.status_code_assertions import AssertionStatusCode
 from src.resources.autentifications.autentificacion import Auth
+from src.assertions.login_assertions import AssertionLogin
 
-from utils.config import BASE_URL
 
 @pytest.mark.login
 @pytest.mark.smoke
 @pytest.mark.regression
-def test_TC55_Login_exitoso_con_credenciales_validas():
+def test_TC55_Auth_exitoso_con_credenciales_validas():
     payload = Auth().get_valid_login_payload()
     response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionLogin.assert_login_input_schema(payload)
     AssertionStatusCode.assert_status_code_200(response)
+    AssertionLogin.assert_login_output_schema(response.json())
 
-def test_TC56_Login_fallido_con_credenciales_invalidas():
+def test_TC56_Auth_fallido_con_credenciales_invalidas():
     payload = Auth().get_invalid_login_payload()
     response = SyliusRequest.post(Endpoint.login(), payload=payload)
     AssertionStatusCode.assert_status_code_401(response)
+
+def test_TC57_Auth_fallido_con_email_invalida():
+    payload = Auth().get_invalid_email_payload()
+    response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionStatusCode.assert_status_code_401(response)
+
+def test_TC58_Auth_fallido_con_password_invalida():
+    payload = Auth().get_invalid_password_payload()
+    response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionStatusCode.assert_status_code_401(response)
+
+def test_TC59_Auth_fallido_con_crendenciales_vacias():
+    payload = Auth().get_empty_credential_payload()
+    response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionStatusCode.assert_status_code_400(response)
+
+def test_TC60_Auth_fallido_con_email_vacia():
+    payload = Auth().get_empty_email_payload()
+    response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionStatusCode.assert_status_code_400(response)
+
+def test_TC61_Auth_fallido_con_password_vacia():
+    payload = Auth().get_empty_password_payload()
+    response = SyliusRequest.post(Endpoint.login(), payload=payload)
+    AssertionStatusCode.assert_status_code_400(response)
