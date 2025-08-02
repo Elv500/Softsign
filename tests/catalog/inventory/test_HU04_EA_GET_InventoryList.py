ad03bb38d7dd8859af8a5dfca4100610cfc96fd0
Elv500 <elvis.alvarez.2001@gmail.com>
2025-08-02 12:08:03 -0400

    Agregando configuración de logging y haciendo prueba en POST

--- tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
+++ tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
@@ -1,4 +1,5 @@
 import pytest
+import logging
 
 from src.assertions.inventory_assertions import AssertionInventory
 from src.assertions.status_code_assertions import AssertionStatusCode
@@ -9,5 +10,10 @@ from src.routes.request import SyliusRequest
 @pytest.mark.regression
 def test_TC23_Obtener_lista_completa_de_fuentes_de_inventario(auth_headers):
     response = SyliusRequest.get(EndpointInventory.inventory(),auth_headers)
+    
+    logging.info("Estatus code: %s", response.status_code)
+    logging.debug("Response: %s", response.json())
+    logging.error("Logging de error!!!")
+
     AssertionStatusCode.assert_status_code_200(response)
     AssertionInventory.assert_inventory_list_schema(response.json())
