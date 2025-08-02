4f3e5702c7b4b440bec4440d4892d74309fbbbb6
Enrique Delgadillo <202000658@est.umss.edu>
2025-08-02 12:36:33 -0400

    HU02-TC114 test: Verificaion de codigo 201 de schema

--- tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
+++ tests/catalog/inventory/test_HU04_EA_GET_InventoryList.py
@@ -1,19 +1,14 @@
 import pytest
-import logging
 
 from src.assertions.inventory_assertions import AssertionInventory
 from src.assertions.status_code_assertions import AssertionStatusCode
-from src.routes.endpoint_inventory import EndpointInventory
-from src.routes.request import SyliusRequest
+from utils.config import BASE_URL
 
 @pytest.mark.smoke
 @pytest.mark.regression
 def test_TC23_Obtener_lista_completa_de_fuentes_de_inventario(auth_headers):
-    response = SyliusRequest.get(EndpointInventory.inventory(),auth_headers)
-    
-    logging.info("Estatus code: %s", response.status_code)
-    logging.debug("Response: %s", response.json())
-    logging.error("Logging de error!!!")
 
+    url = f"{BASE_URL}/admin/inventory-sources"
+    response = requests.get(url, headers=auth_headers)
     AssertionStatusCode.assert_status_code_200(response)
     AssertionInventory.assert_inventory_list_schema(response.json())
