a38264c5ca94e72cb25137e898be5d87d41a3093
Edwinnavia <edwinnavia24@gmail.com>
2025-08-14 18:35:26 -0400

    HU-28-TC385 test: agregando comentarios a los test

--- tests/catalog/association_types/test_HU28_EN_DELETE_ProductAssociationTypes.py
+++ tests/catalog/association_types/test_HU28_EN_DELETE_ProductAssociationTypes.py
@@ -7,6 +7,7 @@ from src.routes.request import SyliusRequest
 from utils.logger_helpers import log_request_response
 
 
+# Catálogo > Association Types - TC_379 Eliminar tipo de asociación existente por código [Exitoso, 204]
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
@@ -24,6 +25,7 @@ def test_TC379_eliminar_tipo_asociacion_existente_por_codigo_exitoso_204(setup_a
     AssertionAssociationTypes.assert_error_schema(response.json())
 
 
+# Catálogo > Association Types - TC_380 Eliminar tipo de asociación con código inexistente [Error 404]
 @pytest.mark.negative
 @pytest.mark.regression
 def test_TC380_eliminar_tipo_asociacion_codigo_inexistente_error_404(auth_headers):
@@ -37,6 +39,7 @@ def test_TC380_eliminar_tipo_asociacion_codigo_inexistente_error_404(auth_header
     AssertionAssociationTypes.assert_error_schema(response.json())
 
 
+# Catálogo > Association Types - TC_381 Enviar solicitud sin token de autorización [Error 401]
 @pytest.mark.security
 @pytest.mark.negative
 @pytest.mark.regression
@@ -50,6 +53,7 @@ def test_TC381_enviar_solicitud_sin_token_autorizacion_error_401(setup_associati
     AssertionStatusCode.assert_status_code_401(response)
 
 
+# Catálogo > Association Types - TC_382 Enviar solicitud con token inválido [Error 401]
 @pytest.mark.security
 @pytest.mark.negative
 @pytest.mark.regression
@@ -65,6 +69,7 @@ def test_TC382_enviar_solicitud_con_token_invalido_error_401(setup_association_t
     AssertionAssociationTypes.assert_invalid_jwt_error(response.json())
 
 
+# Catálogo > Association Types - TC_383 Enviar solicitud sin header Accept-Language [Exitoso, idioma por defecto]
 @pytest.mark.functional
 @pytest.mark.regression
 def test_TC383_eliminar_tipo_asociacion_ya_eliminado(setup_association_types):
@@ -81,6 +86,7 @@ def test_TC383_eliminar_tipo_asociacion_ya_eliminado(setup_association_types):
     AssertionAssociationTypes.assert_error_schema(response_2.json())
 
 
+# Catálogo > Association Types - TC_384 Enviar solicitud sin header Accept
 @pytest.mark.functional
 @pytest.mark.regression
 def test_TC384_enviar_solicitud_sin_header_accept(setup_association_types):
@@ -100,6 +106,7 @@ def test_TC384_enviar_solicitud_sin_header_accept(setup_association_types):
     AssertionAssociationTypes.assert_error_schema(response.json())
 
 
+# Catálogo > Association Types - TC_385 Eliminar tipo de asociación usando el campo `id` en vez de `code` [Error 404]
 @pytest.mark.negative
 @pytest.mark.regression
 def test_TC385_eliminar_tipo_asociacion_usando_id_en_vez_de_code_error_404(setup_association_types):
