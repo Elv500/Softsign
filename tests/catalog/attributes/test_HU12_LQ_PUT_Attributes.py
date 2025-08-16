ce3bf7733b793d88bef218096fc92df89392d3d0
lili-QA28 <maly.24.17.02@gmail.com>
2025-08-15 23:23:04 -0400

    Agregando marks de prioridad a todos los TCs y agregando Loggerrs a los TC 39, y 249

--- tests/catalog/attributes/test_HU12_LQ_PUT_Attributes.py
+++ tests/catalog/attributes/test_HU12_LQ_PUT_Attributes.py
@@ -12,6 +12,7 @@ from utils.logger_helpers import log_request_response
 
 
 #Admin> Catalog> Attributes TC_47: No se debe permitir actualizar si el atributo no existe.
+@pytest.mark.high
 @pytest.mark.functional
 @pytest.mark.smoke
 def test_TC47_Verificar_que_no_permitar_actualizar_un_atributo_inexistente(auth_headers):
@@ -23,8 +24,9 @@ def test_TC47_Verificar_que_no_permitar_actualizar_un_atributo_inexistente(auth_
 
 
 #Admin> Catalog> Attributes TC_48: No se debe permitir actualizar un atributo sin token.
+@pytest.mark.high
 @pytest.mark.security
-@pytest.mark.smoke
+@pytest.mark.negative
 def test_TC48_Verificar_que_no_se_permita_actualizar_atributo_sin_token():
     update_data = generate_attributes_source_data()
     url = EndpointAttributes.code("codigo_inexistente_123")
@@ -34,10 +36,12 @@ def test_TC48_Verificar_que_no_se_permita_actualizar_atributo_sin_token():
 
 
 #Admin> Catalog> Attributes TC_233: No se debe actualizar el atributo con data invalida.
-@pytest.mark.functional
+@pytest.mark.high
+@pytest.mark.negative
 @pytest.mark.smoke
 def test_TC233_Verificar_que_no_se_actualice_atributo_con_datos_invalidos(auth_headers):
     data = generate_attributes_source_data()
+
     post_response = SyliusRequest.post(EndpointAttributes.attributes(), auth_headers, data)
     AssertionStatusCode.assert_status_code_201(post_response)
     code = post_response.json()["code"]
@@ -51,6 +55,7 @@ def test_TC233_Verificar_que_no_se_actualice_atributo_con_datos_invalidos(auth_h
 
 
 # Admin> Catalog> Attributes> TC_44: Se permite actualizar un atributo con datos validos.
+@pytest.mark.high
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_TC44_Verificar_se_actualice_attributo_con_datos_validos(setup_attributes_cleanup):
@@ -72,9 +77,11 @@ def test_TC44_Verificar_se_actualice_attributo_con_datos_validos(setup_attribute
             "maxCharacters": None
         }
     AssertionAttributes.assert_attributes_put_input_schema(update_data)
+    log_request_response(url, create_response, headers=auth_headers, payload=update_data)
 
 
 # Admin> Catalog> Attributes> TC_46: No se debe permitir actualizar un atributo sin el campo name.
+@pytest.mark.high
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_TC46_Verificar_actualizar_grupo_sin_el_campo_name(setup_attributes_cleanup):
@@ -100,8 +107,9 @@ def test_TC46_Verificar_actualizar_grupo_sin_el_campo_name(setup_attributes_clea
 
 
 # Admin> Catalog> Attributes> TC_249: Se permite actualizar un atributo con caracteres especiales en el campo name.
+@pytest.mark.high
 @pytest.mark.functional
-@pytest.mark.smoke
+@pytest.mark.negative
 def test_TC249_Verificar_actualizar_atributo_con_caracteres_especiales_en_name(setup_attributes_cleanup):
     auth_headers, add_attributes_for_cleanup = setup_attributes_cleanup
 
@@ -114,19 +122,20 @@ def test_TC249_Verificar_actualizar_atributo_con_caracteres_especiales_en_name(s
     add_attributes_for_cleanup(attributes_code)
 
     update_data = {
-        "name": "updateName 66$@$@#"
+        "name": "updateName66$@$@#"
     }
     endpoint = EndpointAttributes.code(attributes_code)
     response = SyliusRequest.put(endpoint, auth_headers, update_data)
 
-    log_request_response(endpoint, response, headers=auth_headers, payload=update_data)
+    log_request_response(endpoint, response, auth_headers, payload=update_data)
     AssertionStatusCode.assert_status_code_200(response)
     AssertionAttributes.assert_attributes_put_output_schema(response.json())
 
 
 # Admin> Catalog> Attributes> TC_250: No se permite actualizar un atributo si la autenticacion del token fallo.
+@pytest.mark.high
 @pytest.mark.security
-@pytest.mark.smoke
+@pytest.mark.negative
 def test_TC250_Verificar_no_actualizar_attribute_con_token_invalido():
     codigo_existente = "retail"
     data = {
@@ -141,6 +150,7 @@ def test_TC250_Verificar_no_actualizar_attribute_con_token_invalido():
 
 
 # Admin> Catalog> Attributes> TC_251: No se permite actualizar un atributo si el body-json es incorrecto.
+@pytest.mark.high
 @pytest.mark.functional
 @pytest.mark.smoke
 def test_TC251_Verificar_actualizar_atributo_json_body_incompleto(setup_attributes_cleanup):
@@ -166,6 +176,7 @@ def test_TC251_Verificar_actualizar_atributo_json_body_incompleto(setup_attribut
 
 
 # Admin> Catalog> Attributes> TC_354: No see permite actualizar un atributo si el content type es incorrecto.
+@pytest.mark.high
 @pytest.mark.functional
 @pytest.mark.smoke
 def test_TC354_Verificar_actualizar_atributo_con_content_type_incorrecto(setup_attributes_cleanup):
@@ -197,6 +208,7 @@ def test_TC354_Verificar_actualizar_atributo_con_content_type_incorrecto(setup_a
 
 
 # Admin> Catalog> Attributes> TC_353: Validar los header de respuesta despues de la actualizacion del atributo.
+@pytest.mark.high
 @pytest.mark.functional
 @pytest.mark.smoke
 def test_TC353_Verificar_headers_response_despues_actualizar_attribute(setup_attributes_cleanup):
